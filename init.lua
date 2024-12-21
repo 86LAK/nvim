89c35b42b85d6dda56f270319e0ba4c230fb150e
86LAK <121588258+86LAK@users.noreply.github.com>
2024-12-21 23:37:39 +1000

    Floaterterminal, lsp setup, relative line numbers and other preferences set

--- init.lua
+++ init.lua
@@ -91,7 +91,7 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = false
+vim.g.have_nerd_font = true
 
 -- [[ Setting options ]]
 -- See `:help vim.opt`
@@ -102,7 +102,7 @@ vim.g.have_nerd_font = false
 vim.opt.number = true
 -- You can also add relative line numbers, to help with jumping.
 --  Experiment for yourself to see if you like it!
--- vim.opt.relativenumber = true
+vim.opt.relativenumber = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = 'a'
@@ -176,10 +176,10 @@ vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagn
 vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 
 -- TIP: Disable arrow keys in normal mode
--- vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
--- vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
--- vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
--- vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
+vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
+vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
+vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
+vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
 
 -- Keybinds to make split navigation easier.
 --  Use CTRL+<hjkl> to switch between windows
@@ -243,6 +243,8 @@ require('lazy').setup({
   --    require('gitsigns').setup({ ... })
   --
   -- See `:help gitsigns` to understand what the configuration keys do
+  --
+  'github/copilot.vim',
   { -- Adds git related signs to the gutter, as well as utilities for managing changes
     'lewis6991/gitsigns.nvim',
     opts = {
@@ -588,14 +590,14 @@ require('lazy').setup({
       })
 
       -- Change diagnostic symbols in the sign column (gutter)
-      -- if vim.g.have_nerd_font then
-      --   local signs = { ERROR = '', WARN = '', INFO = '', HINT = '' }
-      --   local diagnostic_signs = {}
-      --   for type, icon in pairs(signs) do
-      --     diagnostic_signs[vim.diagnostic.severity[type]] = icon
-      --   end
-      --   vim.diagnostic.config { signs = { text = diagnostic_signs } }
-      -- end
+      if vim.g.have_nerd_font then
+        local signs = { ERROR = '', WARN = '', INFO = '', HINT = '' }
+        local diagnostic_signs = {}
+        for type, icon in pairs(signs) do
+          diagnostic_signs[vim.diagnostic.severity[type]] = icon
+        end
+        vim.diagnostic.config { signs = { text = diagnostic_signs } }
+      end
 
       -- LSP servers and clients are able to communicate to each other what features they support.
       --  By default, Neovim doesn't support everything that is in the LSP specification.
@@ -626,10 +628,89 @@ require('lazy').setup({
         -- But for many setups, the LSP (`ts_ls`) will work just fine
         -- ts_ls = {},
         --
+        ansiblels = {
+          filetypes = { 'yaml', 'ansible' },
+        },
+
+        bashls = {
+          filetypes = { 'sh', 'bash' },
+        },
+
+        csharp_ls = {
+          filetypes = { 'cs' },
+        },
+
+        css_variables = {
+          filetypes = { 'css', 'scss', 'less' },
+        },
+
+        docker_compose_language_service = {
+          filetypes = { 'dockerfile', 'docker-compose' },
+        },
+
+        dockerls = {
+          filetypes = { 'Dockerfile' },
+        },
+
+        fsautocomplete = {
+          filetypes = { 'fsharp', 'fs', 'fsi', 'fsx', 'ml', 'mli' },
+        },
+
+        golangci_lint_ls = {
+          filetypes = { 'go' },
+        },
+
+        gopls = {
+          filetypes = { 'go' },
+        },
+
+        grammarly = {
+          filetypes = { 'markdown', 'text', 'latex', 'rst' },
+        },
+
+        helm_ls = {
+          filetypes = { 'helm' },
+        },
+
+        html = {
+          filetypes = { 'html', 'htmldjango' },
+        },
+
+        jsonls = {
+          filetypes = { 'json', 'jsonc' },
+        },
+
+        powershell_es = {
+          filetypes = { 'ps1', 'psm1', 'psd1' },
+        },
+
+        pylsp = {
+          filetypes = { 'python' },
+        },
+
+        pylyzer = {
+          filetypes = { 'python' },
+        },
+
+        ruff = {
+          filetypes = { 'ruff' },
+        },
+
+        tailwindcss = {
+          filetypes = { 'html', 'css', 'scss', 'less', 'stylus' },
+        },
+
+        terraformls = {
+          filetypes = { 'hcl', 'tf', 'tfvars' },
+        },
+
+        yamlls = {
+          filetypes = { 'yaml', 'yml' },
+        },
 
         lua_ls = {
           -- cmd = { ... },
-          -- filetypes = { ... },
+          filetypes = { 'lua' },
           -- capabilities = {},
           settings = {
             Lua = {
@@ -926,12 +1007,12 @@ require('lazy').setup({
   --  Here are some example plugins that I've included in the Kickstart repository.
   --  Uncomment any of the lines below to enable them (you will need to restart nvim).
   --
-  -- require 'kickstart.plugins.debug',
-  -- require 'kickstart.plugins.indent_line',
-  -- require 'kickstart.plugins.lint',
-  -- require 'kickstart.plugins.autopairs',
-  -- require 'kickstart.plugins.neo-tree',
-  -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
+  require 'kickstart.plugins.debug',
+  require 'kickstart.plugins.indent_line',
+  require 'kickstart.plugins.lint',
+  require 'kickstart.plugins.autopairs',
+  require 'kickstart.plugins.neo-tree',
+  require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
@@ -964,6 +1045,9 @@ require('lazy').setup({
     },
   },
 })
+
+-- Required to load in custom made plugins
+require 'custom.plugins'
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
