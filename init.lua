86a862558355738dae4c07622a7f161e408cf0bb
86LAK <121588258+86LAK@users.noreply.github.com>
2024-12-22 23:24:44 +1000

    Custom start screen implemented

--- init.lua
+++ init.lua
@@ -254,6 +254,38 @@ require('lazy').setup({
   'github/copilot.vim',
 
   {
+    'folke/snacks.nvim',
+    priority = 1000,
+    lazy = false,
+    opts = {
+      dashboard = {
+        preset = {
+          header = [[
+ █████   ██████  ██       █████  ██   ██ 
+██   ██ ██       ██      ██   ██ ██  ██  
+ █████  ███████  ██      ███████ █████   
+██   ██ ██    ██ ██      ██   ██ ██  ██  
+ █████   ██████  ███████ ██   ██ ██   ██ 
+]],
+          -- stylua: ignore
+          ---@type snacks.dashboard.Item[]
+          keys = {
+            { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+            { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
+            { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
+            { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
+            { icon = " ", key = "c", desc = "Config", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
+            { icon = " ", key = "s", desc = "Restore Session", section = "session" },
+            { icon = " ", key = "x", desc = "Lazy Extras", action = ":LazyExtras" },
+            { icon = "󰒲 ", key = "l", desc = "Lazy", action = ":Lazy" },
+            { icon = " ", key = "q", desc = "Quit", action = ":qa" },
+          },
+        },
+      },
+    },
+  },
+
+  {
     'akinsho/toggleterm.nvim',
     version = '*',
     config = true,
@@ -676,10 +708,6 @@ require('lazy').setup({
 
         gopls = {
           filetypes = { 'go' },
-        },
-
-        grammarly = {
-          filetypes = { 'markdown', 'text', 'latex', 'rst' },
         },
 
         helm_ls = {
